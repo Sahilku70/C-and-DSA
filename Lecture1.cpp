5d9ad6255ddb885f92e674b43805c8109f88c4e9
Sahilku70 <Sahilku707@gmil.com>
2025-08-04 11:09:54 +0530

    Lecture1 Done

---
+++ Lecture1.cpp
@@ -0,0 +1,20 @@
+//First program......
+
+#include<iostream>
+/*
+#include<iostream>
+This line includes the input/output stream library in your program.
+iostream stands for Input/Output Stream and is necessary to use cin (for input) and cout (for output).
+The #include directive tells the compiler to include the contents of the file <iostream> before compiling.*/
+
+using namespace std;      //C++ has different namespaces to avoid name conflicts.
+
+int main(){
+/*
+int main()
+This is the main function where the execution of the program starts.
+int indicates the function returns an integer value (usually 0 if successful).
+The {...} contains the body of the function.
+*/
+    cout<<"Hello World";
+}
