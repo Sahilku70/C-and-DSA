50bac5a03e1d3110ffcbe64b2cf6c73f3f1d1f60
Sahilku70 <Sahilku707@gmil.com>
2025-08-04 20:46:31 +0530

    Leacture2 Done

---
+++ Leacture2.cpp
@@ -0,0 +1,36 @@
+/*               
+Variables:- are containers for storing data values.
+------------------------------------------------------
+Identifiers:-
+All C++ variables must be identified with unique names.These unique names are called identifiers.
+
+Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+---------------------------------------------------------------------------------------------------
+Data Types:- define the type of data a variable can hold.
+
+                                                    Data Types
+                                                        |
+                        ----------------------------------------------------------------
+                        |                               |                              | 
+                        |                               |                              |     
+                    PRIMARY                          DERIVED                     USER DEFINED
+                    |                                   |                              |
+                    |--Integer                          |--Functions                   |--Class
+                    |--Character                        |--Array                       |--Structure
+                    |--Boolean                          |--Pointers                    |--Union
+                    |--Floating Point                   |--Reference                   |--Enum
+                    |--Double floating Point
+                    |--void
+                    |--Wide Character
+
+
+-----------------------------------------------------------------
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    cout<<a;
+}
